cee903f0480c02af18fe33acea0427ae358d8201
Karol Rogoziński <rogkar98@gmail.com>
2022-01-11 00:11:05 +0100

    09.01.2022 v8

--- static/main.js
+++ static/main.js
@@ -9,12 +9,11 @@ console.log(dataFromDB.time)
 
 
 let ctx1 = document.getElementById("myChart1").getContext("2d");
-//let ctx2 = document.getElementById("myChart2").getContext("2d");
+let ctx2 = document.getElementById("myChart2").getContext("2d");
 
 let delayed
 
 let labels = dataFromDB.time
-
 
 let dataTemperature = {
     labels,
@@ -22,6 +21,17 @@ let dataTemperature = {
         data: dataFromDB.temperature,
         label: "temperature",
         borderColor: "green",
+        tension: 0.3,
+    },
+    ],
+};
+
+let dataHumidity = {
+    labels,
+    datasets: [{
+        data: dataFromDB.humidity,
+        label: "humidity",
+        borderColor: "blue",
         tension: 0.3,
     },
     ],
@@ -58,6 +68,38 @@ let configTemperature = {
 
 };
 
+let configHumidity = {
+    type: "line",
+    data: dataHumidity,
+    options: {
+        animation: {
+            onComplete: () => {
+                delayed = true;
+            },
+            delay: (context) => {
+                let delay = 0;
+                if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                    delay = context.dataIndex * 50 + context.datasetIndex * 20;
+                }
+                return delay;
+            },
+        },
+        radius: 2,
+        responsive: true,
+        scales: {
+            y: {
+                ticks: {
+                    callback: function (value) {
+                        return Math.round(value * 10) / 10  + "%";
+                    },
+                },
+            },
+        },
+    },
+
+};
+
 let myChart1 = new Chart(ctx1, configTemperature)
+let myChart2 = new Chart(ctx2, configHumidity)
 
 
