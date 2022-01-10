c5b3ac617a4df4a53aa866439590de00a5b71a29
Karol Rogoziński <rogkar98@gmail.com>
2022-01-10 03:30:47 +0100

    09.01.2022 v8

--- static/main.js
+++ static/main.js
@@ -1,5 +1,61 @@
-let ctx = document.getElementById("myChart").getContext("2d");
+// let ctx = document.getElementById("myChart").getContext("2d");
 
+// let delayed
 
+// let labels = [
+//     '2012',
+//     '2013',
+//     '2014',
+//     '2015',
+//     '2016',
+//     '2017',
+//     '2018',
+//     '2019',
+//     '2020',
+// ];
 
-let myChart = new Chart(ctx, config)
+// let data = {
+//     labels,
+//     datasets: [{
+//         data: [167, 267, 248, 319, 225, 189, 297, 267, 305],
+//         label: "CD Project stock",
+//         borderColor: "green",
+//         tension: 0.3,
+//     },
+//     ],
+// };
+
+// //let data = dataFromDB.temperature
+
+// let config = {
+//     type: "line",
+//     data: data,
+//     options: {
+//         animation: {
+//             onComplete: () => {
+//                 delayed = true;
+//             },
+//             delay: (context) => {
+//                 let delay = 0;
+//                 if (context.type === 'data' && context.mode === 'default' && !delayed) {
+//                     delay = context.dataIndex * 50 + context.datasetIndex * 20;
+//                 }
+//                 return delay;
+//             },
+//         },
+//         radius: 2,
+//         responsive: true,
+//         scales: {
+//             y: {
+//                 ticks: {
+//                     callback: function (value) {
+//                         return value + "°C";
+//                     },
+//                 },
+//             },
+//         },
+//     },
+
+// };
+
+// let myChart = new Chart(ctx, config)
