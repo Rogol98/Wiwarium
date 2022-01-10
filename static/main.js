b7762a2adc0f64f0e4384cd9ee7583b402b17b6c
Karol Rogoziński <rogkar98@gmail.com>
2022-01-10 20:07:56 +0100

    09.01.2022 v8

--- static/main.js
+++ static/main.js
@@ -15,6 +15,8 @@ let labels = [
     '2020',
 ];
 parsee = dataFromDB.replaceAll('&#39;', '\"')
+parsee = dataFromDB.replaceAll(')', '')
+parsee = dataFromDB.replaceAll('(', '')
 console.log(parsee)
 console.log("TYPEOF parsee: " + typeof parsee)
 //parsee = JSON.parse(parsee)
