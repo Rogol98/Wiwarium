e3a404f9c90615e06f28cbc611e22ebb2c613e96
Karol Rogoziński <rogkar98@gmail.com>
2022-01-12 14:05:48 +0100

    09.01.2022 v8

--- static/main.js
+++ static/main.js
@@ -116,38 +116,39 @@ function nextweek() {
     var nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 21);
     return nextweek;
 }
-function showLastDay() {
-    //for (i = ; i < 5; i++) {
-    let yesterday = new Date();
-    //let words = today.split(' ');
-    //console.log("TYPEOF: " + typeof words)
+// function showLastDay() {
+//     let dates = dataFromDB.time
+//     let today = new Date();
+//     let aDayAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate - 1, today.getHours, today.getMinutes, today.getSeconds);
+//     for (i = ; i < 5; i++) {
+//         let yesterday = new Date();
+//         let words = today.split(' ');
+//         console.log("TYPEOF: " + typeof words)
 
 
-}
-let today = dataFromDB.time
-console.log("TYPEOF today: " + typeof today)
-console.log(today)
-console.log(Array.isArray(today))
-console.log(nextweek())
+//     }
 
-myChart1.data.labels.pop()
-myChart1.data.labels.pop()
-myChart1.update()
-myChart2.update()
+    //console.log("TYPEOF today: " + typeof today)
+    console.log(dataFromDB.time)
+
+    myChart1.data.labels.pop()
+    myChart1.data.labels.pop()
+    myChart1.update()
+    myChart2.update()
 
 
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
-}
+    function addData(chart, label, data) {
+        chart.data.labels.push(label);
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+        });
+        chart.update();
+    }
 
-function removeData(chart) {
-    chart.data.labels.pop();
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
-    chart.update();
-}
+    function removeData(chart) {
+        chart.data.labels.pop();
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.pop();
+        });
+        chart.update();
+    }
