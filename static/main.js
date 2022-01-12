88fec9d1c3a68f176283cec395971f4729be020d
Karol Rogoziński <rogkar98@gmail.com>
2022-01-12 16:17:32 +0100

    09.01.2022 v8

--- static/main.js
+++ static/main.js
@@ -119,11 +119,11 @@ function nextweek() {
 function showLastDay() {
     let dates = dataFromDB.time
     let today = new Date();
+
     let aDayAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate - 1, today.getHours, today.getMinutes, today.getSeconds);
     for (i = 0; i < dates.length; i++) {
         console.log("TYPEOF: " + typeof dates[i])
-        console.log(dates[i])//.split(' ').split('-')[2])
-       // let words = today.split(' ');
+        console.log(dates[i].split(' ').split('-')[2])
 
     }
 }
