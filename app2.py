3f43d2dd2b1e906c8ff3135bb777dc81cbda3732
Karol Rogoziński <rogkar98@gmail.com>
2022-01-14 20:48:37 +0100

    09.01.2022 v8

--- app2.py
+++ app2.py
@@ -24,6 +24,7 @@ for pin in pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
 
+
 @app.route("/")
 def main():
     # For each pin, read the pin state and store it in the pins dictionary:
@@ -59,20 +60,23 @@ def action(pinNumber, action):
     templateData = {
         'pins': pins
     }
-
     return render_template('controlPanel.html', **templateData)
 
 
+@app.route("/controlPanel")
+def controlPanel():
+    return render_template('controlPanel.html')
+
 @app.route("/diagrams")
 def diagrams():
-   data = {
-      'temperature' : get_value_from_DB('''SELECT temperature FROM sensors;'''),
-      'humidity' : get_value_from_DB('''SELECT humidity FROM sensors;'''),
-      'soilMoisture' : get_value_from_DB('''SELECT soil_moisture/10.23 FROM sensors;'''),
-      'luminosity' : get_value_from_DB('''SELECT light_intensity FROM sensors;'''),
-      'time' : get_value_from_DB('''SELECT time FROM sensors;'''),
-   }
-   return render_template('diagrams.html', data=data)
+    data = {
+        'temperature': get_value_from_DB('''SELECT temperature FROM sensors;'''),
+        'humidity': get_value_from_DB('''SELECT humidity FROM sensors;'''),
+        'soilMoisture': get_value_from_DB('''SELECT soil_moisture/10.23 FROM sensors;'''),
+        'luminosity': get_value_from_DB('''SELECT light_intensity FROM sensors;'''),
+        'time': get_value_from_DB('''SELECT time FROM sensors;'''),
+    }
+    return render_template('diagrams.html', data=data)
 
 
 def create_connection(db_file):
@@ -93,28 +97,31 @@ def execute_query(conn, query):
     conn.commit()
     return values
 
+
 def get_value_from_DB(query):
     conn = create_connection(database)
     with conn:
         value = execute_query(conn, query)
     return value
 
+
 def main():
-   #  light_intensity
-   #  soil_moisture
-   # humidity
-   # timestamp = time.strftime(('%Y-%m-%d %H:%M:%S'))
+    #  light_intensity
+    #  soil_moisture
+    # humidity
+    # timestamp = time.strftime(('%Y-%m-%d %H:%M:%S'))
     # create a database connection
-   query = ''' SELECT temperature FROM sensors;'''
-   print(get_value_from_DB(query))
-   # query = ''' SELECT humidity FROM sensors;'''
-   # print(get_value_from_DB(query))
-   # query = ''' SELECT soil_moisture FROM sensors;'''
-   # print(get_value_from_DB(query))
-   # query = ''' SELECT light_intensity FROM sensors;'''
-   # print(get_value_from_DB(query))
-   # query = ''' SELECT time FROM sensors;'''
-   # print(get_value_from_DB(query))
+    query = ''' SELECT temperature FROM sensors;'''
+    print(get_value_from_DB(query))
+    # query = ''' SELECT humidity FROM sensors;'''
+    # print(get_value_from_DB(query))
+    # query = ''' SELECT soil_moisture FROM sensors;'''
+    # print(get_value_from_DB(query))
+    # query = ''' SELECT light_intensity FROM sensors;'''
+    # print(get_value_from_DB(query))
+    # query = ''' SELECT time FROM sensors;'''
+    # print(get_value_from_DB(query))
+
 
 if __name__ == "__main__":
     main()
